dee9264015b0502d82c0a8d598f2d40ec2905249
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-05-26 00:56:24 -0400

    Added Sauce topping class Fixed the getCost method in Cheese for represent size cost

--- src/main/java/com/pluralsight/toppings/Cheese.java
+++ src/main/java/com/pluralsight/toppings/Cheese.java
@@ -9,7 +9,20 @@ public class Cheese extends Topping {
     }
 
     public double getCost(String size){
-        double baseCost = .50;
+        double baseCost;
+        switch (size.toLowerCase()) {
+            case "small":
+                baseCost = 0.50;
+                break;
+            case "medium":
+                baseCost = 0.75;
+                break;
+            case "large":
+                baseCost = 1.00;
+                break;
+            default:
+                baseCost = 0.50; // fallback
+        }
         return extra ? baseCost * 2 : baseCost;
     }
 
--- src/main/java/com/pluralsight/toppings/Sauce.java
+++ src/main/java/com/pluralsight/toppings/Sauce.java
@@ -1,4 +1,19 @@
 package com.pluralsight.toppings;
 
-public class Sauce {
+public class Sauce extends Topping {
+
+    public Sauce(String name){
+        super(name);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+
+    @Override
+    public double getCost(String size) {
+        return 0;
+    }
 }
