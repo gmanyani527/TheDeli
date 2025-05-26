c5cf5b7e40627c21ccea9fb25405409fc3ac792d
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-05-26 00:59:03 -0400

    Added the RegularTopping Class

--- src/main/java/com/pluralsight/toppings/RegularTopping.java
+++ src/main/java/com/pluralsight/toppings/RegularTopping.java
@@ -1,4 +1,18 @@
 package com.pluralsight.toppings;
 
-public class RegularTopping {
+public class RegularTopping extends Topping{
+
+    public RegularTopping(String name){
+        super(name);
+    }
+
+    @Override
+    public double getCost(String size) {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
 }
