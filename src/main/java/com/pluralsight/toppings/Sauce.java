16ed660b2517b44ed42bc47a639aee38766ae6fd
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-05-26 02:02:47 -0400

    Edited the Sauce class just alittle

--- src/main/java/com/pluralsight/toppings/Sauce.java
+++ src/main/java/com/pluralsight/toppings/Sauce.java
@@ -1,9 +1,13 @@
 package com.pluralsight.toppings;
 
+import com.pluralsight.enums.SauceType;
 public class Sauce extends Topping {
+    private SauceType type;
 
-    public Sauce(String name){
-        super(name);
+    public Sauce(SauceType type){
+        super(type.name());
+        this.type = type;
+
     }
 
     @Override
