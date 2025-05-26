b34ebaa20fbd97f55badb97cf5289637c4da6c11
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-05-26 01:26:32 -0400

    Add CheeseType enum and update Cheese class to use enum instead of strings for cheese names

---
+++ src/main/java/com/pluralsight/enums/CheeseType.java
@@ -0,0 +1,9 @@
+package com.pluralsight.enums;
+
+public enum CheeseType{
+    CHEDDAR,
+    SWISS,
+    MOZZARELLA,
+    PROVOLONE,
+    AMERICAN
+}
--- src/main/java/com/pluralsight/toppings/Cheese.java
+++ src/main/java/com/pluralsight/toppings/Cheese.java
@@ -1,29 +1,29 @@
 package com.pluralsight.toppings;
 
+import com.pluralsight.enums.CheeseType;
+
 public class Cheese extends Topping {
     private boolean extra;
+    private CheeseType type;
 
-    public Cheese(String name, boolean extra){
-        super(name);
+    public Cheese(CheeseType type, boolean extra){
+        super(type.name());
+        this.type = type;
         this.extra = extra;
     }
 
     public double getCost(String size){
-        double baseCost;
-        switch (size.toLowerCase()) {
-            case "small":
-                baseCost = 0.50;
-                break;
-            case "medium":
-                baseCost = 0.75;
-                break;
-            case "large":
-                baseCost = 1.00;
-                break;
-            default:
-                baseCost = 0.50; // fallback
+        double baseCost = 0;
+
+        // Optionally vary by size
+        if (size.equalsIgnoreCase("Medium")) {
+            baseCost += 0.25;
+        }
+        if (size.equalsIgnoreCase("Large")) {
+            baseCost += 0.50;
         }
         return extra ? baseCost * 2 : baseCost;
+
     }
 
 }
