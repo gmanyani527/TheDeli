4596d1fd5b9b88b6d78ec7ca925c80b1664952b8
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-05-27 00:49:31 -0400

    Added the Chip class

--- src/main/java/com/pluralsight/model/Chips.java
+++ src/main/java/com/pluralsight/model/Chips.java
@@ -1,5 +1,21 @@
 package com.pluralsight.model;
 
 public class Chips {
+    String flavor;
+    Double price;
+
+
+    public Chips(String flavor){
+        this.flavor = flavor;
+        price = 1.50;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
 
 }
