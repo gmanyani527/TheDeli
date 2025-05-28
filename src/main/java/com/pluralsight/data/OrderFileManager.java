21f8d7394bfd53a8a1e3c56eb1eb7d6d13a5789f
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-05-28 00:21:40 -0400

    Add method to create receipts folder if it doesn't exist
    - Checks for existence of 'receipts' directory
    - Creates the folder if missing and confirms status in console
    - Ensures receipt generation has a designated out

--- src/main/java/com/pluralsight/data/OrderFileManager.java
+++ src/main/java/com/pluralsight/data/OrderFileManager.java
@@ -1,4 +1,38 @@
 package com.pluralsight.data;
 
+
+import com.pluralsight.model.Order;
+import com.pluralsight.model.Sandwich;
+import com.pluralsight.model.Drink;
+import com.pluralsight.model.Chips;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class OrderFileManager {
+
+public static void createReceiptsFolder(){
+
+    File receiptFolder = new File("receipts");
+
+    if (!receiptFolder.exists()) {
+        boolean created = receiptFolder.mkdir();
+        if (created) {
+            System.out.println(" Receipts folder created.");
+        } else {
+            System.out.println(" Failed to create 'receipts' folder.");
+        }
+    } else {
+        System.out.println(" Receipts folder already exists.");
+    }
+
+}
+
+
+
+
+
 }
