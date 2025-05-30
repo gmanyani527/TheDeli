13b1bab2fef62034e171fe5b5a1ebdd69f5e0eb1
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-05-29 22:36:11 -0400

    Created VeggieDelight Signature

---
+++ src/main/java/com/pluralsight/signatures/VeggieDelight.java
@@ -0,0 +1,25 @@
+package com.pluralsight.signatures;
+
+import com.pluralsight.enums.*;
+import com.pluralsight.model.Sandwich;
+import com.pluralsight.toppings.*;
+
+import java.util.ArrayList;
+
+public class VeggieDelight extends Sandwich {
+    public VeggieDelight() {
+        super(
+                8, // 8-inch sandwich
+                "Wheat", // bread
+                new ArrayList<>() {{
+                    add(new RegularTopping(RegularToppingType.LETTUCE));
+                    add(new RegularTopping(RegularToppingType.TOMATOES));
+                    add(new RegularTopping(RegularToppingType.CUCUMBERS));
+                    add(new RegularTopping(RegularToppingType.PEPPERS));
+                    add(new RegularTopping(RegularToppingType.PICKLES));
+                    add(new Sauce(SauceType.RANCH));
+                }}
+        );
+        setToasted(false);
+    }
+}
